55dfa5a57aca28a209cbd093b6b1df20c81c6347
jin <bdtjin00@gmail.com>
2025-12-10 22:02:24 +0900

    feat: 1번 과제 추가

--- bin/score_advanced.dart
+++ bin/score_advanced.dart
@@ -1,1 +1,23 @@
-void main(List<String> arguments) {}
+// void main(List<String> arguments) {}
+
+// 부모 클래스
+class Score {
+  String name = '';
+  int score = 90;
+  void showInfo(){
+    print('점수: $score점');
+  }
+}
+// 자식 클래스
+class StudentScore extends Score {
+  @override
+  String name = '홍길동';
+  void showInfo(){
+    print('이름: $name');
+    super.showInfo();
+  }
+}
+void main(){
+  StudentScore studentScore = StudentScore();
+  studentScore.showInfo();
+}
